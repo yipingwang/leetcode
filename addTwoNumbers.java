72d4f05b154c2f9d5eb6c3248e43f43da85f9bac
Yiping Wang <cs.wang.yiping@gmail.com>
2013-11-22 14:36:57 -0600

    Create addTwoNumbers.java

---
+++ addTwoNumbers.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        ListNode result = new ListNode(0);
+        ListNode cur1 = l1, cur2=l2, head=result;
+        int carry=0;
+        while(cur1!=null || cur2!=null){
+             if(cur1!=null){
+                carry+=cur1.val;
+                cur1=cur1.next;
+            }
+             if(cur2!=null){
+                carry+=cur2.val;
+                cur2=cur2.next;
+            }
+           head.next=new ListNode(carry%10);
+           head=head.next;
+           carry/=10;
+        }
+        if(carry==1) head.next=new ListNode(1);
+        
+        return result.next;
+        
+        
+    }
+}
